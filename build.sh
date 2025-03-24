4a4b2eaa0809d7626cf36e74f401356442a6d5a5
Jack.Wang <37522188+integemjack@users.noreply.github.com>
2025-03-24 08:05:46 +0000

    更新构建脚本以创建构建目录并将固件复制到该目录

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 docker run --rm -it -v .:/project -w /project --privileged integem/pydrone:latest bash -c "cd ports/esp32 && make BOARD=PYDRONE"
-
-cp -rf ports/esp32/build-PYDRONE/firmware.bin ./firmware.bin
+mkdir -p build
+cp -rf ports/esp32/build-PYDRONE/firmware.bin ./build/firmware.bin
